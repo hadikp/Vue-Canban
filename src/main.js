afd150895ea848b007c984b4acce3528cb94eb0f
hadikp <hp20001025@gmail.com>
2023-01-13 21:44:17 +0100

    fa icons

--- src/main.js
+++ src/main.js
@@ -3,7 +3,18 @@ import './style.css'
 import App from './App.vue'
 
 import router from './plugins/router'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {faCog, faIgloo, faPlus, faUserAstronaut, faDownload, faUpload, faPencil, faPrint, faThumbTack, faImage, 
+  faSquarePlus, faUser, faTrashAlt, faTimes, faTag, faSortAmountUp, faSortAmountDown, faSitemap, faHomeLg, faTachometerAlt,
+  faSave, faTh, faSmile, faUserPlus, faQuestionCircle} from '@fortawesome/free-solid-svg-icons'
+import {faCalendarCheck} from '@fortawesome/free-regular-svg-icons'  
+
+library.add(faCog, faIgloo, faPlus, faUserAstronaut, faDownload, faUpload, faPencil, faPrint, faThumbTack, faImage,
+  faSquarePlus, faUser, faTrashAlt, faTimes, faTag, faSortAmountUp, faSortAmountDown, faSitemap, faHomeLg, faTachometerAlt,
+  faSave, faTh, faSmile, faUserPlus, faQuestionCircle, faCalendarCheck)
 
 createApp(App)
+  .component('font-awesome-icon', FontAwesomeIcon)
   .use(router)
   .mount('#app')
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -5,6 +5,33 @@
 <template>
   <header></header>
   <main>
+    <font-awesome-icon icon="user-astronaut" />
+    <font-awesome-icon icon="igloo" />
+    <font-awesome-icon icon="plus" />
+    <font-awesome-icon icon="cog" />
+    <font-awesome-icon icon="download" />
+    <font-awesome-icon icon="upload" />
+    <font-awesome-icon icon="pencil" />
+    <font-awesome-icon icon="print" />
+    <font-awesome-icon icon="thumbtack" />
+    <font-awesome-icon icon="image" />
+    <font-awesome-icon icon="square-plus" />
+    <font-awesome-icon icon="user" />
+    <font-awesome-icon icon="trash-alt" />
+    <font-awesome-icon icon="times" />
+    <font-awesome-icon icon="tag" />
+    <font-awesome-icon icon="sort-amount-up" />
+    <font-awesome-icon icon="sort-amount-down" />
+    <font-awesome-icon icon="sitemap" />
+    <font-awesome-icon icon="battery" />
+    <font-awesome-icon icon="home-lg" />
+    <font-awesome-icon icon="tachometer-alt" />
+    <font-awesome-icon icon="save" />
+    <font-awesome-icon icon="th" />
+    <font-awesome-icon class="smile" icon="smile" />
+    <font-awesome-icon icon="user-plus" />
+    <font-awesome-icon icon="question-circle" />
+    <font-awesome-icon icon="fa-regular fa-calendar-check" />
     <h1>Welcome</h1>
   </main>
   <footer></footer>
@@ -12,6 +39,8 @@
 </template>
 
 <style scoped>
-
+  .smile {
+    color: yellow;
+  }
 
 </style>
